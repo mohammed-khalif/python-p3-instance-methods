d76f3b4f4131d395bd98c9f067f2f9e8aff5c12b
mohammed-khalif <mohamed.yusuf@student.moringaschool.com>
2023-05-21 15:27:39 -0400

    completed

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
+    # body class
+    def sit(self):
+        print("The dog is sitting.")
+    
+    def bark(self):
+        print("Woof!")
 
-    #Instance method definition
-    pass
+# an instance of the Dog class
+snoopy = Dog()
+
+# Calling the sit() method on the snoopy instance
+snoopy.sit()
+
+# Calling the bark() method on the snoopy instance
+snoopy.bark()
--- lib/person.py
+++ lib/person.py
@@ -5,3 +5,11 @@ class Person:
 
     #Instance method definition
     pass
+# lib/person.py
+
+class Person:
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
